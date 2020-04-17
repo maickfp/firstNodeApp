30e809e665dbd3d4a468686513912401de363026
Michael Fernandez <michael_f2005@hotmail.com>
2020-04-16 20:34:17 -0500

    Usando Faker

---
+++ README.md
@@ -0,0 +1,5 @@
+# Modulos en Node.JS (Make It Real)
+
+1. IVA
+2. log
+3. Faker
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+// modulos propios
 const {iva, ivaCanastaFamiliar, ivaVehiculos} = require("./modules/iva");
 const log = require("./modules/logs");
+// modulos terceros
+const faker = require("faker")
 
 log.info(`El valor del IVA es: ${iva}%`);
 log.info(`El valor del IVA Vehiculos es: ${ivaVehiculos}%`);
 log.info(`El valor del IVA Canasta Familiar es: ${ivaCanastaFamiliar}%`);
-log.error();
+
+console.log("\n\n");
+
+log.info(faker.name.findName());
+log.info(faker.lorem.words(10));
